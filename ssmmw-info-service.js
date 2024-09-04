69bd4a7601567f2d82759c7bac15e76b113d37bb
meg allen <95981696+meggardiola@users.noreply.github.com>
2024-09-04 17:09:08 +1000

    Update ssmmw-info-service.js

--- ssmmw-info-service.js
+++ ssmmw-info-service.js
@@ -1,20 +1,27 @@
-const app_cnfg = require('./config/ap.config.json');
-const common = require('./lib/common.js');
+const app_cnfg = require("./config/ap.config.json");
+const common = require("./lib/common.js");
 
 process.env.NODE_ENV = app_cnfg.ENV;
 process.env.APP_NAME = app_cnfg.APP_NAME;
 
-process.env.QUEUE_ENV = common.setQueueName(app_cnfg.ENV, app_cnfg.APP_AGENCY, app_cnfg.APP_NAME);
+process.env.QUEUE_ENV = common.setQueueName(
+  app_cnfg.ENV,
+  app_cnfg.APP_AGENCY,
+  app_cnfg.APP_NAME
+);
 process.env.UV_THREADPOOL_SIZE = 128; // test
-const redis_cnfg = require('./config/redis.config.json')[process.env.NODE_ENV || 'DEVT'];
+const redis_cnfg = require("./config/redis.config.json")[
+  process.env.NODE_ENV || "DEVT"
+];
 
 var express = require("express");
-var main = require('./app/routes/main.js');
+var main = require("./app/routes/main.js");
 // var profile = require('./app/routes/profile.js');
 // var entity = require('./app/routes/entity.js');
 // var param = require('./app/routes/param.js');
 // var term = require('./app/routes/term.js')
-var acgs = require('./app/routes/acgs.js');
+var acgs = require("./app/routes/acgs.js");
+var fin = require("./app/routes/fin.js");
 // var cert = require('./app/routes/cert.js');
 // var fincomp = require('./app/routes/fincomp.js');
 // var biz = require('./app/routes/biz.js');
@@ -25,54 +32,69 @@ var acgs = require('./app/routes/acgs.js');
 var app = express();
 
 function REST() {
-    var self = this;
-    self.configureExpress();
-};
+  var self = this;
+  self.configureExpress();
+}
 
 REST.prototype.configureExpress = function () {
-    var self = this;
+  var self = this;
 
-    let i = 0;
-    while (i < app_cnfg.APP_WORKER) {
-        const worker_info = require('./app/redis/worker-info.js')();
-        i++;
-    }
-    console.log('INFO: [QUEUE]', process.env.QUEUE_ENV, redis_cnfg.REDIS_CNFG.redis, i, 'CONSUMERS.');
+  let i = 0;
+  while (i < app_cnfg.APP_WORKER) {
+    const worker_info = require("./app/redis/worker-info.js")();
+    i++;
+  }
+  console.log(
+    "INFO: [QUEUE]",
+    process.env.QUEUE_ENV,
+    redis_cnfg.REDIS_CNFG.redis,
+    i,
+    "CONSUMERS."
+  );
 
-    app.use(express.urlencoded({ extended: true }));
-    app.use(express.json({ limit: '2mb' }));
+  app.use(express.urlencoded({ extended: true }));
+  app.use(express.json({ limit: "2mb" }));
 
-    var router = express.Router();
-    app.use('/ssmmw-info', router);
+  var router = express.Router();
+  app.use("/ssmmw-info", router);
 
-    var main_router = new main(router);
-    var acgs_router = new acgs(router);
+  var main_router = new main(router);
+  var acgs_router = new acgs(router);
+  var fin_router = new fin(router);
 
-    app.get('/', function (req, res) {
-        res.json({ "Message": "SSMMW-INFO-SERVICE" });
-    });
+  app.get("/", function (req, res) {
+    res.json({ Message: "SSMMW-INFO-SERVICE" });
+  });
 
-    app.get('*', function (req, res) {
-        res.json({ errorMsg: 'invalid endpoint url / method', successCode: '11' });
-    })
+  app.get("*", function (req, res) {
+    res.json({ errorMsg: "invalid endpoint url / method", successCode: "11" });
+  });
 
-    self.startServer();
-}
+  self.startServer();
+};
 
 REST.prototype.startServer = function () {
-    app.set('trust proxy', '127.0.0.1');
+  app.set("trust proxy", "127.0.0.1");
 
-    const server = app.listen(app_cnfg.APP_PORT, function () {
-        // set timeout for server
-        server.timeout = app_cnfg.APP_TIMEOUT;
-        console.log('INFO: [APWKR]', process.env.NODE_ENV, app_cnfg.APP_AGENCY, app_cnfg.APP_NAME,
-                    'PORT:', app_cnfg.APP_PORT, 'T/O:', server.timeout);
-    });
-}
+  const server = app.listen(app_cnfg.APP_PORT, function () {
+    // set timeout for server
+    server.timeout = app_cnfg.APP_TIMEOUT;
+    console.log(
+      "INFO: [APWKR]",
+      process.env.NODE_ENV,
+      app_cnfg.APP_AGENCY,
+      app_cnfg.APP_NAME,
+      "PORT:",
+      app_cnfg.APP_PORT,
+      "T/O:",
+      server.timeout
+    );
+  });
+};
 
 REST.prototype.stop = function (err) {
-    console.log("ERRO: ISSUE WITH MYSQL n" + err);
-    // process.exit(1);
-}
+  console.log("ERRO: ISSUE WITH MYSQL n" + err);
+  // process.exit(1);
+};
 
 new REST();
